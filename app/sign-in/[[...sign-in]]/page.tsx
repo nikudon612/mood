bef606f41b534773c1080044eeebbed7127fe5de
Nick Bechtel <bechtel.nick@gmail.com>
2023-08-28 18:11:51 -0400

    updates to styling for sign in sign up

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,12 @@
 import { SignIn } from '@clerk/nextjs'
+import React from 'react'
 
 const SignInPage = () => {
-  return <SignIn />
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <SignIn />
+    </div>
+  )
 }
 
 export default SignInPage
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,12 @@
 import { SignUp } from '@clerk/nextjs'
+import React from 'react'
 
 const SignUpPage = () => {
-  return <SignUp afterSignUpUrl="/new-user" redirectUrl="/new-user" />
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <SignUp afterSignUpUrl="/new-user" redirectUrl="/new-user" />
+    </div>
+  )
 }
 
 export default SignUpPage
